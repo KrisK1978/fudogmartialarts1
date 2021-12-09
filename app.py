fcca5f5a31dd7eb76af0c7f3adbe86d0bfb5c9d0
KrisK1978 <k_kempa@yahoo.co.uk>
2021-12-09 14:34:49 +0000

    Added heading to contact page

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import os
-from flask import Flask
+from flask import (
+    Flask,  render_template, flash, request)
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+
+# About page
+@app.route('/')
+def about():
+    """
+    Renders the homepage
+    """
+    return render_template("pages/about.html")
+
+
+# Contact Us page
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """
+    Renders Contact Us page,
+    allows the logged in user to submit the message
+    """
+    if request.method == "POST":
+        flash(
+            "Thanks {}, we have received your message! We'll be in touch with you shortly!".format(
+                request.form["name"]))
+    return render_template("pages/contact_us.html")
 
 
 if __name__ == '__main__':
